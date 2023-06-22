6e720ecbb2a077f446c7e8986ab7d9c8d2392443
micheljunior-code <137418550+micheljunior-code@users.noreply.github.com>
2023-06-22 11:55:09 -0300

    Add files via upload

---
+++ marques.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marques Construções-Sua reforma só aqui.</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="teste.css">
+</head>
+<body>
+    <div class="background">
+        <header>
+            <nav class="navbar bg-body-tertiary">
+                <div class="container">
+                  <a class="navbar-brand" href="#">
+                    <img src="imagemteste/logo.png" width="100" height="65">
+                  </a>
+                </div>
+              </nav>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#"></a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Início</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Contato</a>
+                      </li>
+                      
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+            </div>
+        </header>
+
+        <div class="Meio">
+            <h1>TEMOS NOVIDADES, DESCONTOS</h1>
+            <h1>TUDO PARA SUA REFORMA</h1>
+            <P>Venha conferir nossos produtos de qualidade</P>
+            <button>Novidades</button>
+        </div>
+
+
+    </div><!--background-->
+    <section>
+        <h1>PRODUTOS EM ALTA</h1>
+        <div class="container-card-1">
+            <div class="cards">
+                <img src="imagemteste/12094_cimento-poty-42-5-kg-keuly_m1_637454743241678418.jpg" >
+                <p>Cimento todas as obras 50kg <br></p>
+                <h6>POTY</h6>
+                <h5>R$ 42,00</h5>
+
+            </div>
+            <div class="cards">
+                <img src="imagemteste/15032.webp" >
+                <p>Caixa D'água 500L <br> </p>
+                <h6>FORTLEV</h6>
+                <h5>R$ 330,00</h5>
+
+            </div>
+            <div class="cards">
+                <img src="imagemteste/porta_de_aluminio_210x80cm_postigo_com_grade_lado_esquerdo_central_aluminio_901_1_8bd0d7971c040f5b6a3c47252b05eb61.webp" >
+                <p>Porta Social 210x90 <br> </p>
+                <h6>Alumínio Branco</h6>
+                <h5>R$ 649,99</h5>
+
+            </div>
+            <div class="cards">
+                <img src="imagemteste/janela-de-correr-lateral-aluminium-sound-block-120x200cm-branca-sasazaki-1280176-foto-20170929142751582_850337_A.png" >
+                <p>Janela de vidro de correr lateral <br> </p>
+                <h6>Alumínio</h6>
+                <h5>R$ 299,99</h5>
+
+            </div>
+            <div class="cards">
+                <img src="imagemteste/10054_assto-astr-soft-branco-tpj-as-br1-3697_l2_637812072768997299.jpg" >
+                <p>Assento Sanitário Oval Branco <br> </p>
+                <h6>ASTRA</h6>
+                <h5>R$ 39,99</h5>
+
+            </div>
+            <div class="cards">
+                <img src="imagemteste/tinta_sbras_acrilica_economica_branco_gelo_18_litros_5051_1_20200507105437.webp" >
+                <p>Tinta Acrilica-Branco gelo 15L<br> </p>
+                <h6>SBRAS</h6>
+                <h5>R$ 98,00</h5>
+
+            </div>
+            <div class="cards">
+                <img src="imagemteste/Piscina-Azul-1000L---Afort_1.png" >
+                <p>Piscina Azul-1000L<br> </p>
+                <h6>AFORT</h6>
+                <h5>R$ 999,99</h5>
+
+            </div>
+            <div class="cards">
+                <img src="imagemteste/download (1).jpg" >
+                <p>Tijolo Furado<br> </p>
+                <h6>CANINDÉ</h6>
+                <h5>R$ 0,55-UND</h5>
+
+            </div>
+            <div class="cards">
+                <img src="imagemteste/images (1).jpg" >
+                <p>Piso Cerâmico brilhante<br> </p>
+                <h6>INCENOR</h6>
+                <h5>R$ 42,00</h5>
+
+            </div>
+            <div class="cards">
+                <img src="imagemteste/votomassa-ac-ii.jpg" >
+                <p>Votomassa AC2-ARGAMASSA<br> </p>
+                <h6>VOTORANTIM</h6>
+                <h5>R$ 19,99</h5>
+                
+            </div>
+           
+        </div>
+        <div class="segundo"><BR><button><b>MAIS PRODUTOS</b></button></div> 
+    </section>
+        <div class="texto-motiva">
+            <p>
+                Material de construção é aqui na <b>Marques Construções.</b> <br><br>
+                Reformar ou construir demanda tempo, organização e dinheiro. Afinal, você precisa se programar e fazer uma lista de<br>
+                <B>materiais de construção</B> para cada etapa de sua obra: desde o marco zero até o momento de colocar os quadros na<br>
+                parede.<br><br>
+                Aqui na Marques Construções você emcontra tudo que precisa para sua construção e reforma. Cimentos, argamassas,<br>
+                aços, blocos, tijolos e todos os acessórios necessários para garantir uma obra de qualidade.<br><br>
+
+            </p>
+        </div>
+    <section class="cta">
+        
+    </section>
+    <section>
+        <h1>NOSSAS NOVIDADES</h1>
+        <div class="container-novidades">
+            <div class="novidades">
+                <img src="imagemteste/ff30dd39bc.webp" width="180" height="160">
+                <p>Kit vaso Sanitário <br> <b>Caixa acoplada e assento</b></p>
+                <h5><b>R$ 499,99</b></h5>
+            </div>
+            <div class="novidades">
+                <img src="imagemteste/MS-250_LUITEX.png" width="180" height="160">
+                <p>Motoserra <br><b>STIHL</b></p>
+                <h5><b>R$ 1.779,99 </b></h5>
+            </div>
+            <div class="novidades">
+                <img src="imagemteste/6969_61509d82-6d9f-4250-82b2-b243fb2c6da1_1100x1100-eb97b61a67efbd000416207333634315-1024-1024.jpg" width="180" height="160">
+                <p>Lustre Pendente <br><b>LUSTRE</b></p>
+                <h5><b>R$ 49,99</b></h5>
+            </div>
+            <div class="novidades">
+                <img src="imagemteste/51P57Xtz47L._AC_UF894,1000_QL80_.jpg" width="180" height="160">
+                <p>Serra Mármore <br><b>BLACK DECKER</b></p>
+                <h5><b>R$ 449,99</b></h5>
+            </div>
+            <div class="novidades">
+                <img src="imagemteste/lampada_led_g_light_6_5w_branca_980669ce-b60f-4b66-84b5-7c4f114c200f.png" width="180" height="160">
+                <p>Lâmpada LED-4,7W<br><b>G-LINGHT</b></p>
+                <h5><b>R$ 6,99</b></h5>
+            </div>
+            <div class="novidades">
+                <img src="imagemteste/15134724395_Design20sem20nome2018.png" width="180" height="160">
+                <p>Lavatório p/banheiro <br> <b>MÁRMORE TRAVERTINO</b></p>
+                <h5><b>R$ 75,00</b></h5>
+            </div>
+        </div>
+    </section>
+    
+        <p class="text-titulo"><b>POR QUE ESCOLHER NOSSA LOJA? </b></p>
+           <div class="text">
+      <img src="imagemteste/mecanico-segurando-algo-sobre-fundo-branco.jpg" width="300" height="200" >
+            <ul class="tex-texto">
+                <li><b>Só trabalhamos com produtos de marcas boas e de ótima qualidade.</b></li>
+                <li><b>Temos ótimos descontos, parcelamos da melhor maneira para caber no seu bolso.</b></li>
+                <li><b>Nosso atendimento é de ótima qualidade, escutamos o cliente para sempre estar trazendo novidades.</b></li>
+                <li><b>Fazemos entrega por toda cidade, você pode comprar em nossa loja ou até mesmo em casa sem sair do seu conforto.</b></li>
+            </ul>
+
+            <p class="text-titulo"><b>QUEM SOMOS? </b></p>
+           <h6 class="sobre">A empresa Marques Construções que tem como razão social Edgar Da Costa Marques foi fundada em 09/05/2016 e<br> está cadastrada na Solutudo no segmento de Materiais para Construção com o CNPJ 24.754.092/0001-63. No<br> mercado, a empresa está localizada na Rua Agronomando Rangel, Nº 471 - Terreo no bairro Centro em Boa Viagem - CE<br>, CEP 63870-000. A empresa Marques Construcoes está cadastrada na Receita Federal sob o CNAE 4744-0/99 com atividade fim de Comércio Varejista De Materiais De Construção Em Geral.</h6>
+
+    </div>
+
+      
+    <section class="contato">
+    
+        
+        <div class="meio-contato">
+            <h3>Fale Conosco</h3>
+            <h5>(88) 98803-1510 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+              </svg></h5>
+              
+            <h5>(88) 99813-0524<br></h5>
+            <h5><b>@marquesconstrubv</b> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+              </svg></h5>
+
+
+        </div>
+        <div class="meio-contato">
+            <h3>Atendimento</h3>
+            <h5>Segunda à sexta: 7hrs às 18hrs</h5>
+            <h5>Sábado: 7hrs às 17hrs</h5>
+            
+        </div>
+        <div class="meio-contato">
+            <h3>Formas de Pagamento</h3>
+            <img src="imagemteste/forma-pagamento-mgv-novo.webp" width="150">
+
+        </div>
+        <div class="meio-contato">
+            <h3>Nossa Localização</h3>
+           <h5>Rua 26 de junho, Boa Viagem, Ceará </h5> 
+           <h5>N° 704</h5>
+           <div class="mapbox">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d993.4708628371753!2d-39.730836683992905!3d-5.122476423823077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7bde9239d6d4a67%3A0x94961dd90e347e5b!2sMarques%20Contruc%C3%B5es!5e0!3m2!1spt-BR!2sbr!4v1686836408975!5m2!1spt-BR!2sbr" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            
+           </div>
+            
+        </div>
+
+       
+    </section>
+    <p class="last-text">Copyright © 2023 Marques Construções,  CNPJ 24.754.092/0001-63. Boa Viagem-CE</p>
+    
+</body>
+</html>
